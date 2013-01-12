23ca8e5dae5ece5449fab2fdc0967ee8ad6a63e2
sasha <alexander.papanek@gmail.com>
2013-01-12 14:14:42 +0000

    added nuget and nunit

--- src/NServiceBus.Xms.Tests/FooTest.cs
+++ src/NServiceBus.Xms.Tests/FooTest.cs
@@ -1,6 +1,14 @@
-﻿namespace NServiceBus.Xms.Tests
+﻿using NUnit.Framework;
+
+namespace NServiceBus.Xms.Tests
 {
+    [TestFixture]
     public class FooTest
     {
+        [Test]
+        public void TestFoo()
+        {
+            Assert.That(new Foo(), Is.Not.Null);
+        }
     }
 }
